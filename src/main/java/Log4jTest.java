e0c2590a36fb7b1b44722b917c86edd16b2a92ad
joyang1 <tingzai.yang@gmail.com>
2018-12-13 11:26:37 +0800

    加入单个打印源配置。

--- src/main/java/Log4jTest.java
+++ src/main/java/Log4jTest.java
@@ -8,7 +8,7 @@ import cn.tommyyang.slf4j4json.logger.Logger;
 public class Log4jTest {
 
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger("file");
+        Logger logger = LoggerFactory.getLogger("kafka_log");
         JsonLogger vinciLog = logger.info().strField("app", "vinci").getLogger();
         vinciLog.strField("index","server-end-log").strField("event", "vinci stream").strField("info", "com.jianshu.logger1111").log();
     }
